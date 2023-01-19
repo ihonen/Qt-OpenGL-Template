d09111c75e2be4f0dae7e27f825e566530e7c240
Anton Ihonen <anton.ihonen@bitwise.fi>
2023-01-19 12:26:03 +0200

    WIP

--- Source/Renderer/Renderer.c
+++ Source/Renderer/Renderer.c
@@ -21,14 +21,16 @@ struct Renderer
 
 // -----------------------------------------------------------------------------
 
-static const char* VertexShaderSource = "#version 330 core\n"
+static const char* VertexShaderSource =
+    "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
     "void main()\n"
     "{\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
     "}\0";
 
-static const char* FragmentShaderSource = "#version 330 core\n"
+static const char* FragmentShaderSource =
+    "#version 330 core\n"
     "out vec4 FragColor;\n"
     "void main()\n"
     "{\n"
